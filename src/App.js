9051890c739105e4088cf12f02d571dd6098ccec
corbaaronknight <54407141+corbaaronknight@users.noreply.github.com>
2019-09-02 20:52:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Awesome!
+          I was changed on featureb.
         </p>
         <a
           className="App-link"
